2261da5c9b7cd799550cfcd9e931e9a067a7454a
Chris Mann <chrispmann@gmail.com>
2022-08-31 10:38:04 -0700

    commit tree 40a8dd765c9c5ac9c7f3a03000021130b293b111

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const element = document.getElementById('main');
+const main = document.querySelector('#main')
+console.log(element);
+
+main.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.textContent = 'Chris is the champion';
+console.log(newHeader);
+
